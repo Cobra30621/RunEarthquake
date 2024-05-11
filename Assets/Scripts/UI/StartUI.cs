b5524c237de3eb6003ef07cda9983385fd0902a1
evan <91110754+evanfan2040@users.noreply.github.com>
2024-05-11 16:26:09 +0800

    UI(Start、End、GameExplan)

---
+++ Assets/Scripts/UI/StartUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame ()
+    {
+
+        SceneManager.LoadScene("Game");
+
+    }
+    public void ExplanGame()
+    {
+        
+        SceneManager.LoadScene("«öÁä»¡©ú");
+    }
+    public void ExitGame()
+    {
+
+        EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
